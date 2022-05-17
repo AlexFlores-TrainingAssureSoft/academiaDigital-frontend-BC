5ddca5c3275c548aab3020ed4efa62d3398e4cb5
Melissa-Andrea <105390573+Melissa-Andrea@users.noreply.github.com>
2022-05-17 10:30:42 -0400

    feature/formTrainee (#6)
    * form
    
    * Update TraineeCreateUpdate.js

--- src/components/trainee/TraineeCreateUpdate.js
+++ src/components/trainee/TraineeCreateUpdate.js
@@ -1,8 +1,16 @@
 import TraineeForm from "./TraineeForm"
+import axios from 'axios';
 
 const CreateUpdateTrainee = () => {
+  const saveTrainee = (trainee) => {
+    axios.post(`https://academia-digital-api-ms.azurewebsites.net/api/v1/PostNewTrainee?firstName=${trainee.firstName}&lastName=${trainee.lastName}&email=${trainee.email}&address=${trainee.address}&mobile=${trainee.mobile}&headTrainer=${trainee.headTrainer}&group=${trainee.group}&feedback=${trainee.feedback}`)
+  .then(() => {
+    console.log('Guardando Trainee', trainee.email)
+  })
+  .catch(error => console.log('error', error))
+  }
   return (
-    <TraineeForm />
+    <TraineeForm saveTrainee={saveTrainee}/>
   )
 }
 

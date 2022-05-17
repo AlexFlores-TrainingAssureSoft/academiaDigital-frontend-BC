cde46d8fa1a22995e29dd9f9e6165da0f6936a99
AlexFlores-TrainingAssureSoft <alex.flores@training.assuresoft.com>
2022-05-16 23:29:08 -0400

    add home and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import Layout from "./routes/Layout";
-import Container from '@mui/material/Container';
 
 function App() {
   return (
-    <Container>
       <Layout />
-    </Container>
   );
 }
 

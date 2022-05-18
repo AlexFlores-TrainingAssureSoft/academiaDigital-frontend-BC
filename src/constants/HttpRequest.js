23c019daed1442b70bcb9a69c9f5525b31f1b5e6
MauricioTrainingAssuresoft <105060790+MauricioTrainingAssuresoft@users.noreply.github.com>
2022-05-17 22:24:35 -0400

    update trainee functionality (#7)

---
+++ src/constants/HttpRequest.js
@@ -0,0 +1,10 @@
+export const api = {
+  academiaDigitalUrl: 'https://academia-digital-api-ms.azurewebsites.net/api/v1',
+};
+
+export const type = {
+  getAllTrainees: '/GetAll',
+  postTrainee: '/PostNewTrainee?',
+  putTrainee: '/UpdateTrainee?',
+  deleteTrainee: '/DeleteById?'
+};
--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TraineeScreen from '../screens/trainee/TraineeListScreen';
 import TraineeCreateScreen from '../screens/trainee/TraineeCreateScreen';
-import TraineeDeatilScreen from '../screens/trainee/TraineeDetailScreen';
+import TraineeUpdateScreen from '../screens/trainee/TraineeUpdateScreen';
 import NavBar from '../components/common/NavBar';
 import HomeIni from '../components/common/Home';
 
@@ -12,9 +12,10 @@ const Layout = () => {
       <div>
         <Routes>
           <Route exact path="/" element={ <HomeIni /> } />
-          <Route exact path="/traineeScreen" element={ <TraineeScreen /> } />
+          <Route exact path="/listTrainee" element={ <TraineeScreen /> } />
           <Route exact path="/createTrainee" element={ <TraineeCreateScreen /> } />
-          <Route exact path="/detailTrainee" element={ <TraineeDeatilScreen /> } />
+          <Route exact path="/updateTrainee" element={ <TraineeUpdateScreen /> } />
+          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </div>
     </Router>
